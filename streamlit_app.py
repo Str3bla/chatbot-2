da37442757c55fc3b89c05bc2db1e556da6ecd96
Str3bla <reportingology@gmail.com>
2025-06-30 20:05:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ import os
 # Self Client Credentials (Recommended for Free Tier)
 ZOHO_CLIENT_ID = "1000.AL6419HX3THLR4U73ZGWVMPGPTITNX"
 ZOHO_CLIENT_SECRET = "361cf827b2cb1e0903843319f0eb7856e3d6fdaac6" 
-ZOHO_REFRESH_TOKEN = "1000.21da772a2bc3ffbe3bfe8f49fa27ca19.b9fd5d0e77e39ae2ae1b33b325bbcb2d"  # You'll get this from the self-client flow
+ZOHO_REFRESH_TOKEN = "your_refresh_token_here"  # You'll get this from the self-client flow
 
 ZOHO_BASE_URL = "https://recruit.zoho.com/recruit/v2"
 TARGET_JOB_OPENING_ID = "ZR_1_JOB"
